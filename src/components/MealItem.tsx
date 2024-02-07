2576a45f85407530c1faf8e1f1d57c7173397ab8
Jacek Wiacek <jacek.wiacek@publicisgroupe.com>
2024-02-07 15:53:19 +0100

    add open and close state to Mealitem

--- src/components/MealItem.tsx
+++ src/components/MealItem.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { Card } from './ui/card';
 
 const MealItem: React.FunctionComponent<{
   name: String;
   ingredients: String;
 }> = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <li>
       <Card className="p-[10px] my-[14px]">
@@ -13,40 +15,42 @@ const MealItem: React.FunctionComponent<{
             <p>{props.ingredients}</p>
           </div>
           <div
+            className="cursor-pointer"
             onClick={() => {
-              console.log('rozszerz');
+              setIsOpen((state) => (state = !state));
             }}
           >
             <i className="fa-solid fa-plus"></i>
           </div>
         </div>
-
-        <div>
+        {isOpen && (
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-              modi repudiandae harum illo sunt assumenda voluptatem amet
-              quisquam. Esse, minima.
-            </p>
-          </div>
-          <div>
-            <div
-              onClick={() => {
-                console.log('edytuj');
-              }}
-            >
-              <i className="fa-solid fa-pen-to-square"></i>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                modi repudiandae harum illo sunt assumenda voluptatem amet
+                quisquam. Esse, minima.
+              </p>
             </div>
-            <div
-              className="recipe-modify-icon recipe-remover"
-              onClick={() => {
-                console.log('usuń');
-              }}
-            >
-              <i className="fa-solid fa-trash"></i>
+            <div>
+              <div
+                onClick={() => {
+                  console.log('edytuj');
+                }}
+              >
+                <i className="fa-solid fa-pen-to-square"></i>
+              </div>
+              <div
+                className="recipe-modify-icon recipe-remover"
+                onClick={() => {
+                  console.log('usuń');
+                }}
+              >
+                <i className="fa-solid fa-trash"></i>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </Card>
     </li>
   );
